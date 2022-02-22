d38b346fc10edbcd274f1524e4c8db5fa901230c
Ivashin-Max <ivashinmax@gmail.com>
2022-02-22 16:50:50 +0300

    404 page

--- front/src/components/pages/NotFound.tsx
+++ front/src/components/pages/NotFound.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
 
-const NotFound = () => {
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
+
+export default function NotFound() {
   return (
-    <div>notFound</div>
-  )
-}
 
-export default NotFound
+    <main>
+      <Box
+        sx={{
+          bgcolor: 'background.paper',
+          pt: 8,
+          pb: 6,
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Ошибка 404
+          </Typography>
+          <Typography variant="h5" align="center" color="text.secondary" paragraph>
+            Страницы с таким адресом не существует
+          </Typography>
+          <Stack
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            <Button
+              component={Link}
+              to='/'
+              variant="contained"
+            >Вернуться на главную</Button>
+          </Stack>
+        </Container>
+      </Box>
+
+    </main>
+
+  );
+}
