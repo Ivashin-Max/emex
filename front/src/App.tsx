0189140b52f34d6233ebf250dbec1957e52f4f07
Ivashin-Max <ivashinmax@gmail.com>
2022-03-03 13:25:50 +0300

    adminPage route

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import Basket from './components/pages/Basket';
 import HomePage from './components/pages/HomePage';
+import RolePage from './components/pages/RolePage';
+import AdminPage from './components/pages/AdminPage';
 import NotFound from './components/pages/NotFound';
 import Header from './components/Header';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -16,7 +18,9 @@ function App() {
 
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<RolePage />} />
+        <Route path="/user" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/basket" element={<Basket />} />
         <Route path="/product" element={<ItemPage />} />
